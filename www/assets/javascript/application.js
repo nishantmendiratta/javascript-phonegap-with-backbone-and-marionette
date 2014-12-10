483ce0c7db536634ab55f339140c7467ecc44ad9
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2014-12-10 19:06:10 +0530

    added content region comments

--- www/assets/javascript/application.js
+++ www/assets/javascript/application.js
@@ -1,6 +1,9 @@
 //include our application within a javascript file
 MyApp = new Backbone.Marionette.Application();
 
+//Region to display elements, it can include header,content,footer,etc..
 MyApp.addRegions({
 	mainRegion: "#content"
 });
+
+//reference - https://github.com/davidsulc/backbone.marionette-collection-example
