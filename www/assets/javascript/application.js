619a41120a28a9063fc2b34e7c466152f5a3b257
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2014-12-10 19:12:10 +0530

    Created Model and Collection

--- www/assets/javascript/application.js
+++ www/assets/javascript/application.js
@@ -1,3 +1,10 @@
+/**
+ * Author - Nishant Mendiratta
+ * Follow @Twitter - MrMendiratta
+ * Reference - https://github.com/davidsulc/backbone.marionette-collection-example
+ */
+
+
 //include our application within a javascript file
 MyApp = new Backbone.Marionette.Application();
 
@@ -6,4 +13,10 @@ MyApp.addRegions({
 	mainRegion: "#content"
 });
 
-//reference - https://github.com/davidsulc/backbone.marionette-collection-example
+//Creating a model (all we need to do is extend a Backbone class)
+AngryDog = Backbone.Model.extend({});
+
+//Creating a collection to store our Dogs
+AngryDogs = Backbone.Collection.extend({
+	model : AngryDog
+});
